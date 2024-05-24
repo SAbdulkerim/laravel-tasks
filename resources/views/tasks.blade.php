0fee73b1528a299e0e5f599f416a6427796a5c15
SAbdulkerim <64261584+SAbdulkerim@users.noreply.github.com>
2024-05-24 15:46:13 +0300

    lightgreen

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-		<div class="col-sm-offset-2 col-sm-8" style="background-color: lightred;">
+		<div class="col-sm-offset-2 col-sm-8" style="background-color: lightgreen ;">
 
             <div class="panel panel-default">
                 <div class="panel-heading">
